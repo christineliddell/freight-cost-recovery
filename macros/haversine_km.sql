6c2f1225f3ec30259b3c9ed2e0cf48bd9b6212c9
Christine Liddell <christinel3383@gmail.com>
2026-07-23 18:45:45 -0400

    Add haversine distance macro

---
+++ macros/haversine_km.sql
@@ -0,0 +1,14 @@
+{% macro haversine_km(lat1, lon1, lat2, lon2) %}
+
+    (
+        6371 * 2 * asin(
+            sqrt(
+                pow(sin(radians({{ lat2 }} - {{ lat1 }}) / 2), 2)
+                + cos(radians({{ lat1 }}))
+                * cos(radians({{ lat2 }}))
+                * pow(sin(radians({{ lon2 }} - {{ lon1 }}) / 2), 2)
+            )
+        )
+    )
+
+{% endmacro %}
